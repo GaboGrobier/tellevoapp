2afa52e383572381b702def4ad9c0ec466fa2d2a
gabriel grobier <gabriel.grobier@gmail.com>
2021-09-16 23:19:09 -0300

    se agrega mensaje de error

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { waitForAsync } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +12,7 @@ import { LoadingController } from '@ionic/angular';
 
 export class DashboardPage implements OnInit {
   user: any;
-  constructor(private activeroute:ActivatedRoute, private router:Router, public loadingController:LoadingController) {
+  constructor(private activeroute:ActivatedRoute, private router:Router, public loadingController:LoadingController,public alertController: AlertController) {
     this.activeroute.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.user=this.router.getCurrentNavigation().extras.state.user;
@@ -22,17 +24,35 @@ export class DashboardPage implements OnInit {
   ngOnInit() {
   }
   cargando(){
-    this.presentLoadingWithOptions()
+    this.presentLoadingWithOptions() 
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Lo sentimos',
+      message: 'No encontramos conductores en la zona que buscas ',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
   async presentLoadingWithOptions() {
     const loading = await this.loadingController.create({
-      spinner: null,
+      spinner: "circles",
       duration: 5000,
       message: 'Buscando Conductores',
       translucent: true,
       cssClass: 'custom-class custom-loading'
     });
-    return await loading.present();
+   await loading.present();
+   const { role, data } = await loading.onDidDismiss();
+   this.presentAlert()
+   return;
+
+    
   }
 
 }
